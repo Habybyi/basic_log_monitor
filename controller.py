6e5680bf4b1771337257f4383ac83e6dfd36f21d
Habybyi <adam.hyben@gmail.com>
2026-01-21 12:14:44 +0100

    reworked adding to activity by ip, added config file

--- controller.py
+++ controller.py
@@ -2,38 +2,18 @@
 import re
 from datetime import datetime
 from datetime import timedelta
+import yaml
 invalid = 0; valid = 0
 words=["failed password"]
-pokusy_ip = {}; pokusy_user = {}; datetimes_ip = {}
 activity = {}
 pattern_invalid=r"Failed password for invalid user (\w+) from (\S+) port (\d+)"; pattern_valid=r"Failed password for (\w+) from (\S+) port (\d+)"
 
-sus_act={}
-def check_times_ip(times, type, user, ip):
-    window = timedelta(minutes=5)
-    limit=3
-    count=0 
-    latest = times[-1]
-    print("type >>", type)
-    start= latest - window
-    print("user >>",user)
+with open('./config.yml',"r") as f:
+    config = yaml.load(f,Loader=yaml.FullLoader)
 
-    for t in times:
-        if t >= start:
-            count += 1
-        if ip not in sus_act:
-            sus_act[ip] = [[user, [t]]]
-        elif ip in sus_act:
-            for i in sus_act[ip]:
-                if user not in i:
-                    sus_act[ip].append([user, [t]])
-                elif user in i:
-                    i[i.index(user)+1].append(t)
-
-    if (type == "valid" and count < 4):
-        return False
-    elif (type == "valid" and count >=4):
-        return True
+def check_algorithm(times, type, user, ip):
+    window = timedelta(config['window'])
+    limit=config['limit']
     
 with open("./test_log.txt","r") as file:
     for line in file:
@@ -55,17 +35,12 @@ with open("./test_log.txt","r") as file:
                 if match:
                     ip = match.group(2); user = match.group(1); port=match.group(3)
                     timestamp = datetime.strptime(f"{datetime.now().year} {month} {day} {time}","%Y %b %d %H:%M:%S")
-                    if user not in activity:
-                        activity[user] = [[ip,[timestamp]]]
-                    else:
-                        found=False
-                        for i in activity[user]:
-                            if i[0] ==ip:
-                                i[1].append(timestamp)
-                                found=True
-                                break
-                        if not found:
-                            activity[user].append([ip,[timestamp]])
+                    if ip not in activity:
+                        activity[ip] = {
+                                'users_tried' : set(),
+                                'timestamps':[],
+                                'types':[]
+                            }
                     print(f"[FAILED][{type.upper()}] {month} {day} {time} - user: {user}, ip: {ip}:{port}")
                 else:
                     print("match was not found")
@@ -76,4 +51,3 @@ with open("./test_log.txt","r") as file:
                     invalid += 1
     print(valid, "VALID")
     print(invalid, "INVALID")
-    print(activity)
